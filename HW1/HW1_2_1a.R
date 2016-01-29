5c37411f4b900d013617d04b4c4e3d82e07f6764
neeraj <neeasthana@gmail.com>
2016-01-28 21:05:57 -0600

    finished HW1 2.1

--- HW1/HW1_2_1a.R
+++ HW1/HW1_2_1a.R
@@ -1,14 +1,23 @@
 library(caret)
+library(klaR)
 
+#load data
 setwd('/home/neeraj/Documents/UIUC/CS 498/CS498MachineLearning/HW1')
 raw_data<-read.csv('pima.csv', header=FALSE)
-
 x_vector <- raw_data[-c(9)]
-y_labels <- raw_data[9]
+y_labels <- raw_data[,9]
 
 #create training and testing sets
-datasplit <- createDataPartition(y=y_labels, p=.8, list=False)
-trainx <- xvector[datasplit,]
-trainy <- yvector[datasplit]
-testx <- xvector[-datasplit]
-testy <- yvector[-datasplit]
+datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
+trainx <- x_vector[datasplit,]
+trainy <- y_labels[datasplit]
+testx <- x_vector[-datasplit,]
+testy <- y_labels[-datasplit]
+
+#train naive bayes model
+tr <- trainControl(method='cv' , number=10)
+model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
+
+#prediction
+predictions <- predict(model, newdata=testx)
+confusionMatrix (data=predictions, testy)
