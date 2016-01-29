b4b78729939004a08384c3ded904611588c28877
neeraj <neeasthana@gmail.com>
2016-01-29 10:11:33 -0600

    Finished implementing HW1 problem 2.1a,b,c,d

--- HW1/HW1_2_1a.R
+++ HW1/HW1_2_1a.R
@@ -1,6 +1,6 @@
 ##Neeraj Asthana (nasthan2)
 ##CS 498 HW1
-
+options(warn=-1)
 library(caret)
 library(klaR)
 
@@ -11,21 +11,45 @@ raw_data<-read.csv('pima.csv', header=FALSE)
 x_vector <- raw_data[-c(9)]
 y_labels <- raw_data[,9]
 
-#create training and testing sets
-datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
-trainx <- x_vector[datasplit,]
-trainy <- y_labels[datasplit]
-testx <- x_vector[-datasplit,]
-testy <- y_labels[-datasplit]
-
 #train naive bayes model
-tr <- trainControl(method='cv' , number=10)
-model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
-
-#prediction
-predictions <- predict(model, newdata=testx)
-confusionMatrix (data=predictions, testy)
-#accuracy: .732
+trscore<-array(dim=10)
+tescore<-array(dim=10)
+for (wi in 1:10){
+  #create training and testing sets
+  datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
+  trainx <- x_vector[datasplit,]
+  trainy <- y_labels[datasplit]
+  testx <- x_vector[-datasplit,]
+  testy <- y_labels[-datasplit]
+  
+  #splitting positive and negative examples
+  trposflag<-trainy>0
+  positive_examples <- trainx[trposflag, ]
+  negative_examples <- trainx[!trposflag,]
+  
+  #calculate means and sds
+  ptrmean<-sapply(positive_examples, mean, na.rm=TRUE)
+  ntrmean<-sapply(negative_examples, mean, na.rm=TRUE)
+  ptrsd<-sapply(positive_examples, sd, na.rm=TRUE)
+  ntrsd<-sapply(negative_examples, sd, na.rm=TRUE)
+  
+  #calculate offsets and scales
+  ptroffsets<-t(t(trainx)-ptrmean)
+  ptrscales<-t(t(ptroffsets)/ptrsd)
+  
+  pteoffsets<-t(t(testx)-ptrmean)
+  ptescales<-t(t(pteoffsets)/ptrsd)
+  ptelogs<--(1/2)*rowSums(apply(ptescales,c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(ptrsd))
+  nteoffsets<-t(t(testx)-ntrmean)
+  ntescales<-t(t(nteoffsets)/ntrsd)
+  ntelogs<--(1/2)*rowSums(apply(ntescales,c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(ntrsd))
+  lvwte<-ptelogs>ntelogs
+  gotright<-lvwte==testy
+  tescore[wi]<-sum(gotright)/(sum(gotright)+sum(!gotright))
+}
+accuracy <- sum(tescore) / length(tescore)
+#accuracy after cross validating 10 times
+accuracy
 
 
 
@@ -38,41 +62,73 @@ for (i in c(3, 4, 6, 8)){
   x_vector_copy[non_values, i]=NA
 }
 
-#create training and testing sets
-datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
-trainx <- x_vector_copy[datasplit,]
-trainy <- y_labels[datasplit]
-testx <- x_vector_copy[-datasplit,]
-testy <- y_labels[-datasplit]
-
 #train naive bayes model
-tr <- trainControl(method='cv' , number=10)
-model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
-
-#prediction
-predictions <- predict(model, newdata=testx)
-confusionMatrix (data=predictions, testy)
-#accuracy: .7582
+trscore<-array(dim=10)
+tescore<-array(dim=10)
+for (wi in 1:10){
+  #create training and testing sets
+  datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
+  trainx <- x_vector_copy[datasplit,]
+  trainy <- y_labels[datasplit]
+  testx <- x_vector_copy[-datasplit,]
+  testy <- y_labels[-datasplit]
+  
+  #splitting positive and negative examples
+  trposflag<-trainy>0
+  positive_examples <- trainx[trposflag, ]
+  negative_examples <- trainx[!trposflag,]
+  
+  #calculate means and sds
+  ptrmean<-sapply(positive_examples, mean, na.rm=TRUE)
+  ntrmean<-sapply(negative_examples, mean, na.rm=TRUE)
+  ptrsd<-sapply(positive_examples, sd, na.rm=TRUE)
+  ntrsd<-sapply(negative_examples, sd, na.rm=TRUE)
+  
+  #calculate offsets and scales
+  ptroffsets<-t(t(trainx)-ptrmean)
+  ptrscales<-t(t(ptroffsets)/ptrsd)
+  
+  pteoffsets<-t(t(testx)-ptrmean)
+  ptescales<-t(t(pteoffsets)/ptrsd)
+  ptelogs<--(1/2)*rowSums(apply(ptescales,c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(ptrsd))
+  nteoffsets<-t(t(testx)-ntrmean)
+  ntescales<-t(t(nteoffsets)/ntrsd)
+  ntelogs<--(1/2)*rowSums(apply(ntescales,c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(ntrsd))
+  lvwte<-ptelogs>ntelogs
+  gotright<-lvwte==testy
+  tescore[wi]<-sum(gotright)/(sum(gotright)+sum(!gotright))
+}
+accuracy <- sum(tescore) / length(tescore)
+#accuracy after cross validating 10 times
+accuracy
 
 
 
 
 ##2.1c
 #create training and testing sets
-datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
-trainx <- x_vector[datasplit,]
-trainy <- y_labels[datasplit]
-testx <- x_vector[-datasplit,]
-testy <- y_labels[-datasplit]
+test_accuracies <- array(dim=10)
+for (wi in 1:10){
+  datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
+  trainx <- x_vector[datasplit,]
+  trainy <- y_labels[datasplit]
+  testx <- x_vector[-datasplit,]
+  testy <- y_labels[-datasplit]
 
-#train naive bayes model using klaR package
-tr <- trainControl(method='cv' , number=10)
-model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
+  #train naive bayes model using klaR package
+  tr <- trainControl(method='cv' , number=10)
+  model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
 
-#prediction
-predictions <- predict(model, newdata=testx)
-confusionMatrix (data=predictions, testy)
-#accuracy: .732
+  #prediction
+  predictions <- predict(model, newdata=testx)
+  correct <- length(testy[testy == predictions])
+  wrong <- length(testy[testy != predictions])
+  accuracy <- correct / (correct + wrong)
+  test_accuracies[wi] <- accuracy
+}
+cross_validation_accuracy <- sum(test_accuracies)/length(test_accuracies)
+#accuracy after cross validating 10 times
+cross_validation_accuracy
 
 
 
