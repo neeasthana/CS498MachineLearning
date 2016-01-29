34acac2136f1283c3c8f7beaff32c6dcba3bc064
neeraj <neeasthana@gmail.com>
2016-01-28 21:47:39 -0600

    finished 2.1c and code to change 0's to NAs for 2.1b

--- HW1/HW1_2_1a.R
+++ HW1/HW1_2_1a.R
@@ -1,6 +1,10 @@
+##Neeraj Asthana (nasthan2)
+##CS 498 HW1
+
 library(caret)
 library(klaR)
 
+##2.1a
 #load data
 setwd('/home/neeraj/Documents/UIUC/CS 498/CS498MachineLearning/HW1')
 raw_data<-read.csv('pima.csv', header=FALSE)
@@ -21,3 +25,14 @@ model <- train (trainx , factor(trainy) , 'nb' , trControl=tr)
 #prediction
 predictions <- predict(model, newdata=testx)
 confusionMatrix (data=predictions, testy)
+
+
+
+##2.1b
+#replace '0' in columns 
+x_vector_copy <- x_vector
+for (i in c(3, 4, 6, 8)){
+  non_values <- x_vector[, i]==0
+  x_vector_copy[non_values, i]=NA
+}
+
