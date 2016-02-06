2255204cbd697609a19c6eee1404e68ae4f11cde
neeraj <neeasthana@gmail.com>
2016-02-06 17:55:31 -0600

    added code to create train, test, and validation sets

--- HW2/HW2.R
+++ HW2/HW2.R
@@ -20,6 +20,26 @@ raw_train_data <- read.csv('adult.data', header=FALSE, na.strings = "?")
 raw_test_data <- read.csv('adult.test', header=FALSE, na.strings = "?")
 raw_data <- rbind(raw_train_data, raw_test_data)
 
+#Split continuous and label data
+x_vector <- raw_data[,c(1,3,5,11,12,13)]
+y_labels <- raw_data[,15]
+
+#Mentions of positive and negative examples for reference
+neg_example <- raw_test_data[2,15]
+pos_example <- raw_test_data[4,15]
+
+#split data into training, test, and validation sets
+datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
+trainx <- x_vector[datasplit,]
+trainy <- y_labels[datasplit]
+otherx <- x_vector[-datasplit,]
+othery <- y_labels[-datasplit]
+datasplit2 <- createDataPartition(y=othery, p=.5, list=FALSE)
+testx <- otherx[datasplit2,]
+testy <- othery[datasplit2]
+valx <- otherx[-datasplit2,]
+valy <- othery[-datasplit2]
+
 ##Problem 2.5a
 #continuous variables: age, fnlwgt, education-num, capital-gain, capital-loss, hours-per-week
 
