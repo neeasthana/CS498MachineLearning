cb80a7af03615014bcabfa14c161614c92846b2e
neeasthana <neeasthana@gmail.com>
2016-02-08 01:19:52 -0600

    Completed gradient descent code. Still need predictions and accuracies

--- HW2/HW2.R
+++ HW2/HW2.R
@@ -14,11 +14,13 @@ percent_validation <- .1
 percent_test <- .1
 num_examples_epoch_test <- 50
 steps_til_eval <- 30
+steplength_a <- 1
+steplength_b <- 1
 
 #read files and create data set
 raw_train_data <- read.csv('adult.data', header=FALSE, na.strings = "?")
 raw_test_data <- read.csv('adult.test', header=FALSE, na.strings = "?")
-raw_data <- rbind(raw_train_data, raw_test_data)
+raw_data <- rbind(raw_train_data, raw_test_data, make.row.names=FALSE)
 
 #Split continuous and label data
 #continuous variables: age, fnlwgt, education-num, capital-gain, capital-loss, hours-per-week
@@ -26,8 +28,10 @@ x_vector <- raw_data[,c(1,3,5,11,12,13)]
 y_labels <- raw_data[,15]
 
 #Mentions of positive and negative examples for reference
-neg_example <- raw_test_data[2,15]
-pos_example <- raw_test_data[4,15]
+neg_example <- y_labels[1]
+neg_example2 <- y_labels[48842]
+pos_example <- y_labels[8]
+pos_example2 <- y_labels[48843]
 
 #split data into training, test, and validation sets
 datasplit <- createDataPartition(y=y_labels, p=.8, list=FALSE)
@@ -46,14 +50,57 @@ hinge_loss <- function(predicted, actual){
   return (max(0, 1 - (predicted * actual) ))
 }
 
-step <- function(lambda){
-  k <- sample(1:length(trainx), 1)
-  xex <- trainx[k]
-  yex <- trainy[k]
+evaluate <- function(x, a, b){
+  new_x <- as.numeric(as.matrix(x))
+  return (t(a) %*% new_x + b) 
 }
 
-for (i in lambdas){
-  print(i)
+converty <- function(y){
+  if(y == neg_example | y == neg_example2){
+    return (-1)
+  }
+  else if(y == pos_example | y == pos_example2){
+    return (1)
+  }
+  else{
+    return(NA)
+  }
+}
+
+for (lambda in lambdas){
+  #random initialization
+  a <- runif(dim(x_vector)[2], min=-.001, max=.001)
+  b <- runif(1, min=0, max=.01)
+  
+  ran_vals <- sample(1:dim(trainx)[1], 50)
+  accuracy_data <- trainx[ran_vals, ]
+  accuracy_labels <- trainy[ran_vals]
+  train_data <- trainx[-ran_vals,]
+  train_labels <- trainy[-ran_vals]
+  
+  for (epoch in 1:epochs){
+    for (step in 1:steps){
+      k <- sample(1:length(train_labels), 1)
+      xex <- as.numeric(as.matrix( train_data[k,] ))
+      yex <- converty( train_labels[k] )
+      
+      pred <- evaluate(xex, a, b)
+      steplength = 1 / ((steplength_a * epoch) + steplength_b)
+      
+      if(yex * pred >= 1){
+        p1 <- lambda * a
+        p2 <- 0
+      }
+      else {
+        p1 <- (lambda * a) - (yex * xex)
+        p2 <- -(yex)
+      }
+      
+      #update values for a and b by gradient descent
+      a <- a - (steplength * p1)
+      b <- b - (steplength * p2)
+    }
+  }
 }
 
 ##Problem 2.5b
