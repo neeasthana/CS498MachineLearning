dac21990823df526fee1420a822997a868ff5dfc
neeasthana <neeasthana@gmail.com>
2016-02-25 15:16:45 -0600

    clean commit before branch

--- HW4/HW4.R
+++ HW4/HW4.R
@@ -2,8 +2,11 @@
 ##CS 498 HW4
 
 ##Environment Setup
+setwd('/home/neeraj/Documents/UIUC/CS 498/CS498MachineLearning/HW4')
 
 ##Problem 3.4
+#Read data
+iris_data <- read.csv("iris.data", header=FALSE)
 
 ##Problem 3.5
 
