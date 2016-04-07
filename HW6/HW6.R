1c761b30e36ee3d8b1ae9b1822c9c8f39c1dbcb1
Neeraj Asthana <nasthan2@illinois.edu>
2016-04-07 14:23:57 -0500

    removed unneccesarry comments at the end

--- HW6/HW6.R
+++ HW6/HW6.R
@@ -119,17 +119,21 @@ num_features <- dim(raw_data)[2]
 #Simple unregularized Logistic regression
 logfit <- glm(y~x, family = "binomial")
 summary(logfit)
-logfit$deviance
+logfit$deviance #report deviance
+par(mfrow=c(2,2))
+plot(logfit)
+
+#comparison logistic regression
+simplelr <- cv.glmnet(x,y, alpha=0,family='binomial', type.measure="class", lambda = c(1e-12, 1e-11)) #ridge
+print(simplelr$cvm)
+
 
 # train models
-res0 <- cv.glmnet(x,y, alpha=0, type.measure = "class",family='binomial') #ridge
+par(mfrow=c(1,1))
+res0 <- cv.glmnet(x,y, alpha=0,family='binomial') #ridge
 res2 <- cv.glmnet(x,y, alpha=.2, family='binomial')
 res4 <- cv.glmnet(x,y, alpha=.4, family='binomial')
 res5 <- cv.glmnet(x,y, alpha=.5, family='binomial')
 res6 <- cv.glmnet(x,y, alpha=.6, family='binomial')
 res8 <- cv.glmnet(x,y, alpha=.8, family='binomial')
 res1 <- cv.glmnet(x,y, alpha=1, family='binomial') #lasso
-
-#predictions
-
-#see how many are misclassified
