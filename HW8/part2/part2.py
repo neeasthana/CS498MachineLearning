92c775e0b4fe5da4dcb1f9178d979123772ecc0e
Neeraj Asthana <nasthan2@illinois.edu>
2016-05-05 15:50:18 -0500

    created proper file strucutre and added specified file to respective folders

---
+++ HW8/part2/part2.py
@@ -0,0 +1,185 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
+flags.DEFINE_float('dropout', 0.9, 'Keep probability for training dropout.')
+flags.DEFINE_string('summaries_dir', '/tmp/mnist_logs', 'Summaries directory')
+
+
+def train():
+    # Import data and create session
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    sess = tf.InteractiveSession()
+
+    # Create a multilayer model.
+
+    # Input placeholders
+    with tf.name_scope('input'):
+        x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+        image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+        tf.image_summary('input', image_shaped_input, 10)
+        y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
+        keep_prob = tf.placeholder(tf.float32)
+        tf.scalar_summary('dropout_keep_probability', keep_prob)
+
+    # We can't initialize these variables to 0 - the network will get stuck.
+    def weight_variable(shape):
+        """Create a weight variable with appropriate initialization."""
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(shape):
+        """Create a bias variable with appropriate initialization."""
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    #Strides of 1 and use 2*2 blocks
+    def conv2d(x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+    def variable_summaries(var, name):
+        """Attach a lot of summaries to a Tensor."""
+        with tf.name_scope('summaries'):
+            mean = tf.reduce_mean(var)
+            tf.scalar_summary('mean/' + name, mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+            tf.scalar_summary('sttdev/' + name, stddev)
+            tf.scalar_summary('max/' + name, tf.reduce_max(var))
+            tf.scalar_summary('min/' + name, tf.reduce_min(var))
+            tf.histogram_summary(name, var)
+
+    def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+        """Reusable code for making a simple neural net layer.
+        It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+        It also sets up name scoping so that the resultant graph is easy to read, and
+        adds a number of summary ops.
+        """
+        # Adding a name scope ensures logical grouping of the layers in the graph.
+        with tf.name_scope(layer_name):
+            # This Variable will hold the state of the weights for the layer
+            with tf.name_scope('weights'):
+                weights = weight_variable([input_dim, output_dim])
+                variable_summaries(weights, layer_name + '/weights')
+            with tf.name_scope('biases'):
+                biases = bias_variable([output_dim])
+                variable_summaries(biases, layer_name + '/biases')
+            with tf.name_scope('Wx_plus_b'):
+                preactivate = tf.matmul(input_tensor, weights) + biases
+                tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+            activations = act(preactivate, 'activation')
+            tf.histogram_summary(layer_name + '/activations', activations)
+            return activations
+
+    def conv_layer(input_tensor, dim, output_dim, layer_name, act=tf.nn.relu):
+        with tf.name_scope(layer_name):
+            # This Variable will hold the state of the weights for the layer
+            with tf.name_scope('weights'):
+                weights = weight_variable(dim)
+                variable_summaries(weights, layer_name + '/weights')
+            with tf.name_scope('biases'):
+                biases = bias_variable([output_dim])
+                variable_summaries(biases, layer_name + '/biases')
+            with tf.name_scope('Wx_plus_b'):
+                h_conv1 = tf.nn.relu(conv2d(input_tensor, weights) + biases)
+                preactivate = max_pool_2x2(h_conv1)
+                tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+            activations = act(preactivate, 'activation')
+            tf.histogram_summary(layer_name + '/activations', activations)
+            return activations
+
+    #input
+    x_image = tf.reshape(x, [-1,28,28,1])
+
+    #layer 1
+    layer1 = conv_layer(x_image, [10, 10, 1, 8], 8, 'layer1')
+
+
+    #second layer
+    layer2 = conv_layer(layer1, [10, 10, 8, 16], 16, 'layer2')
+
+    #third layer
+    layer3 = conv_layer(layer2, [7, 7, 16, 32], 32, 'layer3')
+
+    #Densely connected layer with all 1024 neurons
+    reshaping = tf.reshape(layer3, [-1, 4*4*32])
+    denselayer = nn_layer(reshaping, 4*4*32,1024, 'dense')
+    #W_fc1 = weight_variable([12 * 12 * 32, 1024])
+    #b_fc1 = bias_variable([1024])
+
+    #h_pool2_flat = tf.reshape(layer2, [-1, 7*7*64])
+    #h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+    #Implement Dropout
+    h_fc1_drop = tf.nn.dropout(denselayer, keep_prob)
+
+    #Softmax Readout layer
+    y = nn_layer(h_fc1_drop, 1024, 10, 'output', act=tf.nn.softmax)
+
+    with tf.name_scope('cross_entropy'):
+        diff = y_ * tf.log(y)
+        with tf.name_scope('total'):
+            cross_entropy = -tf.reduce_mean(diff)
+        tf.scalar_summary('cross entropy', cross_entropy)
+
+    with tf.name_scope('train'):
+        train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy)
+
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary('accuracy', accuracy)
+
+    # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+    merged = tf.merge_all_summaries()
+    train_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/train', sess.graph)
+    test_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/test')
+    tf.initialize_all_variables().run()
+
+    # Train the model, and also write summaries.
+    # Every 10th step, measure test-set accuracy, and write test summaries
+    # All other steps, run train_step on training data, & add training summaries
+    def feed_dict(train):
+        """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
+        if train:
+            xs, ys = mnist.train.next_batch(100)
+            k = FLAGS.dropout
+        else:
+            xs, ys = mnist.test.images, mnist.test.labels
+            k = 1.0
+        return {x: xs, y_: ys, keep_prob: k}
+
+    #num steps=2000
+    for i in range(2001):
+        # Record summaries and test-set accuracy every 100 steps
+        if i % 100 == 0:
+            summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
+            test_writer.add_summary(summary, i)
+            print('Accuracy at step %s: %s' % (i, acc))
+        else: # Record train set summarieis, and train
+            summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+            train_writer.add_summary(summary, i)
+
+def main(_):
+    if tf.gfile.Exists(FLAGS.summaries_dir):
+        tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
+    tf.gfile.MakeDirs(FLAGS.summaries_dir)
+    train()
+
+if __name__ == '__main__':
+    tf.app.run()
